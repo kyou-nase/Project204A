5dbed6cf0c350fe92bd43d76aefa71d02a38a19e
kyou <67418265+kyou-nase@users.noreply.github.com>
2023-03-03 18:55:48 +0800

    0.0.1

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,4 @@
-// 	Jiju Airlines Reservation v.1.1.5
+// Project204A - 0.0.1
 
 #include <iostream> 	//library of standard input/output streams
 #include <iomanip> 		//library containing functions manipluting outputs such as setw and setprecision
@@ -6,9 +6,6 @@
 #include <ctime> 		//Library that contain functions to get and use date and time information based from the local system
 #include <windows.h> 	//Library that contains declarations for all of the functions in the Windows API SetConsoleCursorPosition, SetConsoleCursor, gotoxy, GetAsyncKeyState
 #include <string>		//Library that allows us to use and manipulate string data types
-
-#pragma warning(disable: 4996)      //To disable IDE specific errors (Visual Studio, VS Code)
-#pragma comment(lib, "User32.lib")  //To link Windows 32-bit library for windows.h 
 
 using namespace std;
 
@@ -121,13 +118,13 @@ int main() {
     
 	string schedTime, numPassngers_input, dateInput, destination, ageInput, baggageInput, payInput, logo = "\t\t\t\t\t  \xc9\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbb\n\t\t\t\t\t  \xba       \xdf\xdf\xdb \xdf\xdb\xdb\xdf  \xdf\xdf\xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba         \xdb  \xde\xdd     \xdb \xdb  \xdb      \xba\n   \t\t\t\t\t  \xba      \xdf\xdc\xdc\xdf \xdc\xdb\xdb\xdc \xdf\xdc\xdc\xdf \xdf\xdc\xdc\xdf      \xba\n \t\t\t\t\t  \xba A    I    R    L    I    N   E\xba\n\t\t\t\t\t  \xba     R E S E R V A T I O N S   \xba\n\t\t\t\t\t  \xc8\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbc";
 
-	time_t timeNow = time(0); 				//Using ctime library, get current time from local system
+	time_t timeNow = time(0); //Using ctime library, get current time from local system
 	tm* localTime = localtime(&timeNow);
 
 	// Input: Welcome Screen [Y/N]
 	system("Title Test Program");
 	system("cls");
-	system("color f5"); //Set the main color of text and backround of program to Light White Backround and Light Blue Texts
+	system("color f1"); //Set the main color of text and backround of program to Light White Backround and Light Blue Texts
 	cout << logo;
 	cout << "\n\n\n\t\t\t\t" << char(201);      for (int i = 0; i < 51; i++) { cout << char(205); }      cout << char(187) << endl;
 	cout << "\t\t\t\t" << char(186); Color(240); cout << "                                                   "; Color(241); cout << char(186) << endl;
